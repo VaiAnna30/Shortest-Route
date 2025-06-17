2a28ba7db75e58e99b0eb99067e2dcc8d1549c06
Vaibhav Tiwari <vaibtiwari.3003@gmail.com>
2025-06-17 22:33:07 +0530

    Update randommm.py

--- randommm.py
+++ randommm.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-random.seed(42)
+# random.seed(42)
 
 # Must be ≥ num_nodes - 1 to ensure connectivity
 num_nodes = 50
@@ -13,6 +13,7 @@ assert num_edges >= num_nodes - 1, "Not enough edges to ensure connectivity."
 # Generate unique node coordinates (id, x, y)
 nodes = []
 for i in range(num_nodes):
+    # Here x and y are storing rangom number b/w 0 to 100 upto 2 decimal places
     x = round(random.uniform(0, 100), 2)
     y = round(random.uniform(0, 100), 2)
     nodes.append((i, x, y))
@@ -22,7 +23,9 @@ for i in range(num_nodes):
 def edge_weight(i, j):
     x1, y1 = nodes[i][1], nodes[i][2]
     x2, y2 = nodes[j][1], nodes[j][2]
-    dist = math.hypot(x2 - x1, y2 - y1)
+    #calculating Distance
+    dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    # adding some noise indicating trafic lights
     return round(dist + random.uniform(0, 10), 2)
 
 # Step 1: Ensure connectivity using a randomized Prim-like method
